d36e53f8c41ad62d219c902a3354930e9d78e887
eyx0 <65051086+eyx0@users.noreply.github.com>
2020-12-02 10:05:29 -0500

    Add simple calculator

---
+++ calculator.py
@@ -0,0 +1,56 @@
+from math import *
+
+print("Dors Calculator")
+
+class EquationError(Exception):
+    pass
+
+def get_equation_parts(array):
+    global number_1
+    global number_2
+    global operator
+
+    if len(array) == 1:
+        operator = array[0]
+    elif len(array) == 2:
+        operator = array[0]
+        number_1 = float(array[1])
+    elif len(array) == 3:
+        operator = array[1]
+        number_1 = float(array[0])
+        number_2 = float(array[2])
+    else:
+        raise EquationError
+
+def process_equation():
+    equation = input("Enter an equation: ")
+    equation_parts = equation.split(" ")
+    return equation_parts
+
+while True:
+    try:
+        get_equation_parts(process_equation())
+        if operator == "+":
+            answer = number_1 + number_2
+        if operator == "-":
+            answer = number_1 - number_2
+        if operator == "*":
+            answer = number_1 * number_2
+        if operator == "/":
+            answer = number_1 / number_2
+        if operator == "^":
+            answer = number_1 ** number_2
+        if operator.lower() == "sqrt":
+            answer = sqrt(number_1)
+        if operator.lower() == "mod":
+            answer = number_1 % number_2
+        if operator.lower() == "exit":
+            break
+
+        print(answer)
+
+    except KeyboardInterrupt:
+        break
+
+    except:
+        print("Invalid equation.")
